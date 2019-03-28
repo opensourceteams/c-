c63e5ce99a30b993477d0259d08ee8d1392881cb
thinktothings <thinktothings@gmail.com>
2019-03-28 13:50:11 +0800

    //字符串拼接     printf("%s --> %s\n","打印输出字符串","第二个字符串");

--- cpp/example_snprintf.cpp
+++ cpp/example_snprintf.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-const int THREAD_NUM = 1; // 测试的线程数，最大不能超过10
+const int THREAD_NUM = 2; // 测试的线程数，最大不能超过10
 char file_names[THREAD_NUM][256]; // 每个线程识别的文件名
 
 char audio_dir[256] = "./pcm"; // 与THREAD_NUM一起决定测试的文件名
